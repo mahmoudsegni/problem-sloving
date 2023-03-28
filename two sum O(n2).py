7dd36e7db53506dd979f389ddeb016972b99fa3a
mahmoudsegni <57681167+mahmoudsegni@users.noreply.github.com>
2023-03-28 03:10:51 +0100

    Initial commit

---
+++ two sum O(n2).py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+          if (target-nums[i] in nums[i+1:]):
+            temp=nums[i+1:]
+            return([i,i+1+temp.index(target-nums[i])])
