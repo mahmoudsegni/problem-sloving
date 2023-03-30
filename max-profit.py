935eb53156d811928a1aad3b2e499a93a8997e43
mahmoudsegni <57681167+mahmoudsegni@users.noreply.github.com>
2023-03-31 00:05:04 +0100

    comment code

--- max-profit.py
+++ max-profit.py
@@ -1,9 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit=0
-        buy=float('inf')
-        for p in prices:
-            buy=min(buy,p)
-            profit=max(profit,p-buy)
-        return(profit)
-        
+        profit=0  # variable to store the maximum profit
+        buy=float('inf')  # variable to store the minimum buying price, initialized as positive infinity
+        for p in prices:  # loop through each price in the list of prices
+            buy=min(buy,p)  # update the minimum buying price
+            profit=max(profit,p-buy)  # calculate the profit by selling at the current price and buying at the minimum buying price
+        return(profit)  # return the maximum profit
